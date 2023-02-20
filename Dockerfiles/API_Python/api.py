dc83e2477714d97930add1687d5c187622092079
rustybridge <giwrgos.vasilop@gmail.com>
2023-02-21 00:14:52 +0200

    Publishing the first part of the project, the deployments/set work, the networking needs to be reworked

---
+++ Dockerfiles/API_Python/api.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from cassandra.cluster import Cluster
+
+app = Flask(__name__)
+api = Api(app)
+
+class read(Resource):
+    def get(self):
+        
+        cluster = Cluster(['my-db-0'],port=9042)
+        session = cluster.connect('my_keyspace')
+        session.execute('USE my_keyspace')
+        data = session.execute('SELECT * FROM my_keyspace.tf_db;')
+        
+        for value in data:
+            response = value.tf_db_quantity
+            return response, 200
+
+class write(Resource):
+    def get(self):
+        cluster = Cluster(['my-db-0'],port=9042)
+        session = cluster.connect('my_keyspace')
+        session.execute('USE my_keyspace')
+        data = session.execute('SELECT * FROM my_keyspace.tf_db;')
+    
+        for value in data:
+            new_value = value.tf_db_quantity + 1
+            session.execute(f"UPDATE tf_db SET tf_db_quantity={new_value} WHERE tf_db_vcounter='view-count';")
+            return new_value, 200
+
+api.add_resource(read, '/read')
+api.add_resource(write, '/write')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
